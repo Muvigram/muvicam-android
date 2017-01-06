503dc132bfdfd7c9abc01cbcd248ab4cd458809d
jaylim <iejisung@gmail.com>
2017-01-06 14:31:32 +0900

    testtest

--- app/src/main/java/com/estsoft/muvicam/ui/home/HomeActivity.java
+++ app/src/main/java/com/estsoft/muvicam/ui/home/HomeActivity.java
@@ -48,8 +48,10 @@ public class HomeActivity extends BaseActivity {
   private final static int PAGE_MUSIC = 0;
   private final static int PAGE_CAMERA = 1;
 
+
   private HomeComponent mHomeComponent;
 
+  
   public static Intent newIntent(Context packageContext) {
     return new Intent(packageContext, HomeActivity.class);
   }
