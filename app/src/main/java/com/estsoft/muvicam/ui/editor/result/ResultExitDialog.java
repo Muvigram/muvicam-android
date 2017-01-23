0da18307f77e0401b8906e53e8cc93026f99fd66
SongYeonSOO <lucklys324@gmail.com>
2017-01-23 17:00:56 +0900

    Change Home button Dialog UI

--- app/src/main/java/com/estsoft/muvicam/ui/editor/result/ResultExitDialog.java
+++ app/src/main/java/com/estsoft/muvicam/ui/editor/result/ResultExitDialog.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.estsoft.muvicam.R;
 import com.estsoft.muvicam.ui.home.HomeActivity;
@@ -18,7 +20,7 @@ import com.estsoft.muvicam.ui.home.HomeActivity;
 
 public class ResultExitDialog extends Dialog {
     LinearLayout editorToHome;
-    ImageView editorToHomeNo, editorToHomeYes;
+    TextView editorToHomeNo, editorToHomeYes;
     Context context;
     public ResultExitDialog(Context context) {
         super(context);
@@ -31,12 +33,13 @@ public class ResultExitDialog extends Dialog {
         WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();
         lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         lpWindow.dimAmount = 0.8f;
-        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+       getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         getWindow().setAttributes(lpWindow);
         setContentView(R.layout.layout_dialog_editor_result_to_home);
+
         editorToHome = (LinearLayout) findViewById(R.id.editor_result_ask_to_home);
-        editorToHomeYes = (ImageView) findViewById(R.id.editor_result_ask_to_home_yes);
-        editorToHomeNo = (ImageView) findViewById(R.id.editor_result_ask_to_home_no);
+        editorToHomeYes = (TextView) findViewById(R.id.editor_result_ask_to_home_yes);
+        editorToHomeNo = (TextView) findViewById(R.id.editor_result_ask_to_home_no);
         editorToHomeYes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
