d2ceb6b972b408ef6beb7a089ac505ff5ea2001d
jaylim <iejisung@gmail.com>
2017-01-25 10:16:09 +0900

    bugfix - prevent items in librarymusic module from being clicked several time

--- app/src/main/java/com/estsoft/muvicam/ui/library/musiclibrary/MusicCutDialogFragment.java
+++ app/src/main/java/com/estsoft/muvicam/ui/library/musiclibrary/MusicCutDialogFragment.java
@@ -2,15 +2,10 @@ package com.estsoft.muvicam.ui.library.musiclibrary;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,12 +19,10 @@ import com.estsoft.muvicam.util.MusicPlayer;
 import com.estsoft.muvicam.util.RxUtil;
 import com.estsoft.muvicam.util.UnitConversionUtil;
 
-import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -74,6 +67,12 @@ public class MusicCutDialogFragment extends DialogFragment {
     super.onCreate(savedInstanceState);
     mMusicPlayer = new MusicPlayer(getActivity(), "silence_15_sec.mp3");
     mMusicPlayer.openPlayer();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mOnPreparedListener.onPrepared();
   }
 
   @Override
@@ -137,6 +136,7 @@ public class MusicCutDialogFragment extends DialogFragment {
     return builder.create();
   }
 
+  //subscription
   Subscription mSubscription;
 
   public WaveformView.WaveformListener mWaveformListener = new WaveformView.WaveformListener() {
@@ -187,5 +187,14 @@ public class MusicCutDialogFragment extends DialogFragment {
         );
   }
 
+  // On Prepared Listener
+  private OnPreparedListener mOnPreparedListener;
 
+  public interface OnPreparedListener {
+    void onPrepared();
+  }
+
+  public void setOnPreparedListener(OnPreparedListener onPreparedListener) {
+    mOnPreparedListener = onPreparedListener;
+  }
 }
