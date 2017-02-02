f0f395f640b8519f91f0fe94511fe43052c8e91c
jaylim <iejisung@gmail.com>
2017-02-02 13:52:49 +0900

    finishing event bus

--- app/src/main/java/com/estsoft/muvicam/util/RxEventBus.java
+++ app/src/main/java/com/estsoft/muvicam/util/RxEventBus.java
@@ -1,6 +1,8 @@
 package com.estsoft.muvicam.util;
 
+import rx.Observable;
 import rx.Subscription;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.subjects.PublishSubject;
 import rx.subjects.SerializedSubject;
@@ -12,18 +14,45 @@ import rx.subjects.Subject;
 
 public class RxEventBus {
 
-  private PublishSubject<Object> mSbject = PublishSubject.create();
+  private final Subject<Event, Event> mBusSubject = new SerializedSubject<>(PublishSubject.create());
 
-  private final Subject<Object, Object> mBusSubject = new SerializedSubject<>(PublishSubject.create());
-
-  public <T> Subscription register(final Class<T> eventClass, Action1<T> onNext) {
-    return mBusSubject
-        .filter(event -> event.getClass().equals(eventClass))
-        .map(obj -> (T) obj)
-        .subscribe(onNext);
+  public Observable<Event> register() {
+    return mBusSubject;
   }
 
-  public void post(Object event) {
+  public <T extends Event> Subscription register(final Class<T> eventClass, Action1<T> onNext, Action1<Throwable> onError, Action0 onComplete) {
+    //noinspection unchecked
+    return mBusSubject
+        .filter(event -> event.getClass().equals(eventClass))
+        .map(event -> (T) event)
+        .subscribe(onNext, onError, onComplete);
+  }
+
+  public void unregister(Subscription subscription) {
+    RxUtil.unsubscribe(subscription);
+  }
+
+  public void post(Event event) {
     mBusSubject.onNext(event);
+  }
+
+  /////
+
+//  private final Subject<Object, Object> bus = new SerializedSubject<>(PublishSubject.create());
+//
+//  public void send(Object o) {
+//    bus.onNext(o);
+//  }
+//
+//  public Observable<Object> toObserverable() {
+//    return bus;
+//  }
+//
+//  public boolean hasObservers() {
+//    return bus.hasObservers();
+//  }
+
+  public static class Event {
+
   }
 }
