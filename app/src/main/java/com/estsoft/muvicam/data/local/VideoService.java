80bc2ed2f35cfa7c85f2bab763e060ac5495535c
jaylim <iejisung@gmail.com>
2017-02-10 14:09:22 +0900

    resolved resolution issue, allowing only up to FHD videos.

--- app/src/main/java/com/estsoft/muvicam/data/local/VideoService.java
+++ app/src/main/java/com/estsoft/muvicam/data/local/VideoService.java
@@ -18,116 +18,113 @@ import rx.Observable;
  */
 
 public class VideoService {
-    private static final String TAG = "VideoService";
-    private static final String ASC = " ASC";
-    private static final String DESC = " DESC";
+  private static final String TAG = "VideoService";
+  private static final String ASC = " ASC";
+  private static final String DESC = " DESC";
 
-    private Context mContext;
+  private Context mContext;
 
-    public VideoService( Context context ) {
-        mContext = context;
+  public VideoService(Context context) {
+    mContext = context;
+  }
+
+  private int mPathIndex;
+  private int mImageIdIndex;
+  private int mDurationIndex;
+  private int mWidthIndex;
+  private int mHeghtIndex;
+  private int mResolutionIndex;
+
+
+  private void initCursorIndex(Cursor cursor) {
+    mPathIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DATA);
+    mImageIdIndex = cursor.getColumnIndex(MediaStore.MediaColumns._ID);
+    mDurationIndex = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DURATION);
+    mWidthIndex = cursor.getColumnIndex(MediaStore.Video.VideoColumns.WIDTH);
+    mHeghtIndex = cursor.getColumnIndex(MediaStore.Video.VideoColumns.HEIGHT);
+    mResolutionIndex = cursor.getColumnIndex(MediaStore.Video.VideoColumns.RESOLUTION);
+  }
+
+  public Observable<VideoMetaData> getVideos(boolean idOrderToDESC) {
+
+    String order;
+    if (idOrderToDESC) order = DESC;
+    else order = ASC;
+
+    Cursor cursor = mContext.getContentResolver().query(
+        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+        null, null, null,
+        MediaStore.Video.Media._ID + order
+        , null
+    );
+
+    initCursorIndex(cursor);
+    if (cursor == null || !cursor.moveToFirst()) {
+      return null;
+    }
+    return CursorObservable.create(cursor, true)
+        .filter(this::isValidFormat)
+        .filter(this::isCapableResolution)
+        .map(this::createVideoMetadata);
+  }
+
+  private VideoMetaData createVideoMetadata(Cursor cursor) {
+    return new VideoMetaData(
+        getThumbnailBitmap(cursor.getInt(mImageIdIndex), mContext),
+        cursor.getInt(mWidthIndex),
+        cursor.getInt(mHeghtIndex),
+        cursor.getInt(mDurationIndex),
+        cursor.getInt(mDurationIndex) / 1000,
+        cursor.getString(mPathIndex),
+        0
+    );
+  }
+
+  private boolean isValidFormat(Cursor cursor) {
+    String path = cursor.getString(mPathIndex);
+    return path.endsWith(".mp4");
+  }
+
+  private boolean isCapableResolution(Cursor cursor) {
+    String resolution = cursor.getString(mResolutionIndex);
+    String[] dimen = resolution.split("x");
+    int x = Integer.parseInt(dimen[0]);
+    int y = Integer.parseInt(dimen[1]);
+
+    return Math.min(x, y) <= 1080;
+  }
+
+  private Bitmap getThumbnailBitmap(int imageId, Context context) {
+    return MediaStore.Video.Thumbnails.getThumbnail(
+        context.getContentResolver(),
+        imageId,
+        MediaStore.Images.Thumbnails.MINI_KIND,
+        null);
+  }
+
+  public static class VideoMetaData {
+    public Bitmap thumbnailBitmap;
+    public String videoPath;
+    public long durationMs;
+    public int durationSec;
+    public int position;
+    public int width;
+    public int height;
+
+    private VideoMetaData(Bitmap thumbnailBitmap, int width, int height, long durationMs, int durationSec, String videoPath, int position) {
+      this.thumbnailBitmap = thumbnailBitmap;
+      this.width = width;
+      this.height = height;
+      this.durationMs = durationMs;
+      this.durationSec = durationSec;
+      this.videoPath = videoPath;
+      this.position = position;
     }
 
-    private int mPathIndex;
-    private int mImageIdIndex;
-    private int mDurationIndex;
-    private int mWidthIndex;
-    private int mHeghtIndex;
-//    private int mResolutionIndex;
-
-    private boolean stopped;
-
-    private void initCursorIndex( Cursor cursor ) {
-        mPathIndex = cursor.getColumnIndex( MediaStore.MediaColumns.DATA );
-        mImageIdIndex = cursor.getColumnIndex( MediaStore.MediaColumns._ID );
-        mDurationIndex = cursor.getColumnIndex( MediaStore.Video.VideoColumns.DURATION );
-        mWidthIndex = cursor.getColumnIndex( MediaStore.Video.VideoColumns.WIDTH );
-        mHeghtIndex = cursor.getColumnIndex( MediaStore.Video.VideoColumns.HEIGHT );
-//        mResolutionIndex = cursor.getColumnIndex( MediaStore.Video.VideoColumns.RESOLUTION );
+    @Override
+    public String toString() {
+      return videoPath + " - " + durationMs + " = " + durationSec + " - " + position + " " + width + " . " + height;
     }
-
-    public Observable<VideoMetaData> getVideos( boolean idOrderToDESC ) {
-
-        String order;
-        if (idOrderToDESC) order = DESC;
-        else order = ASC;
-
-            Cursor cursor = mContext.getContentResolver().query(
-                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                    null, null, null,
-                    MediaStore.Video.Media._ID + order
-                    , null
-            );
-
-            initCursorIndex( cursor );
-            if (cursor == null || !cursor.moveToFirst()) {
-                return null;
-            }
-        return CursorObservable.create(cursor, true)
-//                .doOnUnsubscribe( this::stopExtracting )
-//                .doOnSubscribe( this::init )
-//                .doOnCompleted( this::init )
-                .filter( this::isValid )
-                .map(cursor1 -> getVideoMetadata(cursor1) );
-    }
-
-    private void init() {
-        stopped = false;
-    }
-    private void stopExtracting() {
-        stopped = true;
-    }
-
-    private VideoMetaData getVideoMetadata( Cursor cursor ) {
-//        if ( this.stopped ) return null;
-        return new VideoMetaData(
-                getThumbnailBitmap( cursor.getInt(mImageIdIndex), mContext ),
-                cursor.getInt(mWidthIndex),
-                cursor.getInt(mHeghtIndex),
-                cursor.getInt(mDurationIndex),
-                cursor.getInt(mDurationIndex) / 1000,
-                cursor.getString(mPathIndex),
-                0
-        );
-    }
-
-    private boolean isValid( Cursor cursor ) {
-        String path = cursor.getString( mPathIndex );
-        Log.d( TAG, "isValid: extracted " + path );
-        return path.endsWith(".mp4") && !path.contains("camera_video_test");
-    }
-
-    private Bitmap getThumbnailBitmap( int imageId, Context context ) {
-        return MediaStore.Video.Thumbnails.getThumbnail(
-                context.getContentResolver(),
-                imageId,
-                MediaStore.Images.Thumbnails.MINI_KIND,
-                null);
-    }
-
-    public static class VideoMetaData {
-        public Bitmap thumbnailBitmap;
-        public String videoPath;
-        public long durationMs;
-        public int durationSec;
-        public int position;
-        public int width;
-        public int height;
-
-        private VideoMetaData(Bitmap thumbnailBitmap, int width, int height, long durationMs, int durationSec, String videoPath, int position) {
-            this.thumbnailBitmap = thumbnailBitmap;
-            this.width = width;
-            this.height = height;
-            this.durationMs = durationMs;
-            this.durationSec = durationSec;
-            this.videoPath = videoPath;
-            this.position = position;
-        }
-
-        @Override
-        public String toString() {
-            return videoPath + " - " + durationMs + " = " + durationSec + " - " + position + " " + width + " . " + height;
-        }
-    }
+  }
 
 }
